427a94a59cc47dc903e73d7f56f785a54d68ed87
jeffjarratt <jeffjarratt@gmail.com>
2025-09-21 12:46:44 -0500

    Update flake.nix

--- flake.nix
+++ flake.nix
@@ -51,8 +51,8 @@
     inherit (self) outputs;
     settings = {
       # User configuration
-      username = "zer0"; # automatically set with install.sh and live-install.sh
-      editor = "nixvim"; # nixvim, vscode, helix, nvchad, neovim, emacs (WIP)
+      username = "birb"; # automatically set with install.sh and live-install.sh
+      editor = "vscode"; # nixvim, vscode, helix, nvchad, neovim, emacs (WIP)
       browser = "zen"; # firefox, floorp, zen
       terminal = "kitty"; # kitty, alacritty, wezterm
       terminalFileManager = "yazi"; # yazi or lf
@@ -60,13 +60,13 @@
       wallpaper = "kurzgesagt"; # see modules/themes/wallpapers
 
       # System configuration
-      videoDriver = "nvidia"; # CHOOSE YOUR GPU DRIVERS (nvidia, amdgpu or intel)
-      hostname = "NixOS"; # CHOOSE A HOSTNAME HERE
-      locale = "en_GB.UTF-8"; # CHOOSE YOUR LOCALE
-      timezone = "Europe/London"; # CHOOSE YOUR TIMEZONE
-      kbdLayout = "gb"; # CHOOSE YOUR KEYBOARD LAYOUT
+      videoDriver = "amdgpu"; # CHOOSE YOUR GPU DRIVERS (nvidia, amdgpu or intel)
+      hostname = "BirbNix"; # CHOOSE A HOSTNAME HERE
+      locale = "en_US.UTF-8"; # CHOOSE YOUR LOCALE
+      timezone = "America/Chicago"; # CHOOSE YOUR TIMEZONE
+      kbdLayout = "us"; # CHOOSE YOUR KEYBOARD LAYOUT
       kbdVariant = "extd"; # CHOOSE YOUR KEYBOARD VARIANT (Can leave empty)
-      consoleKeymap = "uk"; # CHOOSE YOUR CONSOLE KEYMAP (Affects the tty?)
+      #consoleKeymap = "uk"; # CHOOSE YOUR CONSOLE KEYMAP (Affects the tty?)
     };
 
     systems = [
